017adb8941ab637f4237b342d3cc8954f5c554d9
huseynli <seyfeddinqarayev2@gmail.com>
2025-11-29 14:26:07 +0400

    Refactor list_all function to simplify return

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+This module contains a function that lists all documents in a MongoDB collection.
+"""
+
 
 def list_all(mongo_collection):
     """
@@ -10,6 +14,4 @@ def list_all(mongo_collection):
     Returns:
         list: List of all documents in the collection. Returns an empty list if none.
     """
-    if not mongo_collection:
-        return []
     return list(mongo_collection.find())
